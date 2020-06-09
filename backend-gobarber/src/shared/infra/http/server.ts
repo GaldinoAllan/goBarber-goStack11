f25122503bfe7edbb9b3b1b3fe244c7e966d9bda
Allan <allan.g.galdino@outlook.com>
2020-06-09 13:48:42 -0300

    folders refactored to conform with DDD pattern

--- backend-gobarber/src/shared/infra/http/server.ts
+++ backend-gobarber/src/shared/infra/http/server.ts
@@ -1,13 +1,14 @@
 import 'reflect-metadata';
+
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
 
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
 
-import './database';
+import '@shared/infra/typeorm';
 
 const app = express();
 
