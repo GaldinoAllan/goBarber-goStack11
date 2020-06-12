a3bf067b1f6760f9fc2e5f4f051e4c17ccfd4ae7
Allan <allan.g.galdino@outlook.com>
2020-06-12 17:42:12 -0300

    tests foi reset password done and token migration

--- api-gobarber/src/modules/users/repositories/IUserTokensRepository.ts
+++ api-gobarber/src/modules/users/repositories/IUserTokensRepository.ts
@@ -3,4 +3,5 @@ import UserToken from '../infra/typeorm/entities/UserToken'
 // eslint-disable-next-line prettier/prettier
 export default interface IUserTokensRepository {
   generate(user_id: string): Promise<UserToken>;
+  findByToken(token: string): Promise<UserToken | undefined>;
 }
--- api-gobarber/src/shared/infra/http/routes/index.ts
+++ api-gobarber/src/shared/infra/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import appointmentRouter from '@modules/appointments/infra/http/routes/apointments.routes';
 import userRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/session.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentRouter);
 routes.use('/users', userRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
