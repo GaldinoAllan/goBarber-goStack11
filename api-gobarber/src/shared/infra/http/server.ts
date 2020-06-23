d2708da7091286756cd431fff4831ed34f45462a
Allan <allan.g.galdino@outlook.com>
2020-06-22 22:32:03 -0300

    🚀 API done an brute force interrupted

--- api-gobarber/src/shared/infra/http/server.ts
+++ api-gobarber/src/shared/infra/http/server.ts
@@ -12,11 +12,12 @@ import routes from './routes';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
+import rateLimiter from './middlewares/RateLimiter';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
-
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
